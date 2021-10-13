518d60571cac14c7be9fc9affc866da5c5f37331
Trevor Conklin <91498857+TrevConk@users.noreply.github.com>
2021-10-13 19:19:55 +0000

    changed while and try statment

--- main.py
+++ main.py
@@ -32,13 +32,13 @@ class Joystick:
   def getY(self, address):
     self.adc.read(1)
 
-while(True):
-  try:
+try:
+  while(True):
     print(Joystick().getX())
     print(Joystick().getY())
     time.sleep(.1)
-  except:
-    print("keyboard interrupt")
+except Exception as e:
+  print(e)
   
 
   
