d824627d5e70b7dee9696126607a2613600f1e23
Trevor Conklin <91498857+TrevConk@users.noreply.github.com>
2021-10-13 19:39:06 +0000

    meme

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ joy = Joystick(0x40)
 
 try:
   while(True):
-    print(joy.getX, joy.getY)
+    print('{:>3} {:0>3}'.format(joy.getX, joy.getY))
     time.sleep(.1)
 except Exception as e:
   print(e)
