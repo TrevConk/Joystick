941cb5e78da2b50408c42b3161e5c66575994431
TConk <91498857+TrevConk@users.noreply.github.com>
2021-10-13 19:01:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import smbus
+import time
+
+class PCF8591:
+
+  def __init__(self,address):
+    self.bus = smbus.SMBus(1)
+    self.address = address
+
+  def read(self,chn): #channel
+      try:
+          self.bus.write_byte(self.address, 0x40 | chn)  # 01000000
+          self.bus.read_byte(self.address) # dummy read to start conversion
+      except Exception as e:
+          print ("Address: %s \n%s" % (self.address,e))
+      return self.bus.read_byte(self.address)
+
+  def write(self,val):
+      try:
+          self.bus.write_byte_data(self.address, 0x40, int(val))
+      except Exception as e:
+          print ("Error: Device address: 0x%2X \n%s" % (self.address,e))
+
+class Joystick:
+  
+  def __init__(self,adc):
+    self.adc = PCF8591(address)
+
+  def getX(self,address):
+    self.adc.read(0)
+
+  def getY(self, address):
+    self.adc.read(1)
+  
+
+  
+
